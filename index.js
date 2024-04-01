995feba86e6355e11cb54812cd21c267fab80f37
Phinocio <github@phinocio.com>
2024-04-01 14:08:50 -0600

    feat: added some console logging heh

--- index.js
+++ index.js
@@ -75,6 +75,13 @@ cron.schedule("0 14 * * *", async () => {
 		}
 
 		channel.send(`Join the Game Day Thread: ${thread}`);
+		console.log("Game Day Thread created");
+	} else {
+		console.log("Game Day Thread not created");
+		console.log(
+			formatInTimeZone(new Date(), "America/Denver", "yyyy-MM-dd") ===
+				formatInTimeZone(game.startTimeUTC, "America/Denver", "yyyy-MM-dd")
+		);
 	}
 });
 
