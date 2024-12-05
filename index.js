d6d74b11ea0296e82f8fd4a849a923a42ca7e487
Devon DeJohn <35853209+ddejohn@users.noreply.github.com>
2024-12-05 16:08:57 -0700

    fix editor formatting

--- index.js
+++ index.js
@@ -11,14 +11,14 @@ const { sarcasmify } = require("./src/helpers/sarcasmify");
 
 // Create a new client instance
 const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMembers,
-    GatewayIntentBits.GuildPresences,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent,
-  ],
-  presence: { activities: [{ type: ActivityType.Watching, name: "Georgiev let in four" }], status: "online" },
+	intents: [
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMembers,
+		GatewayIntentBits.GuildPresences,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.MessageContent,
+	],
+	presence: { activities: [{ type: ActivityType.Watching, name: "Georgiev let in four" }], status: "online" },
 });
 
 client.commands = new Collection();
@@ -26,71 +26,71 @@ const foldersPath = path.join(__dirname, "src/commands");
 const commandFolders = fs.readdirSync(foldersPath);
 
 for (const folder of commandFolders) {
-  const commandsPath = path.join(foldersPath, folder);
-  const commandFiles = fs.readdirSync(commandsPath).filter((file) => file.endsWith(".js"));
-  for (const file of commandFiles) {
-    const filePath = path.join(commandsPath, file);
-    const command = require(filePath);
-    if ("data" in command && "execute" in command) {
-      client.commands.set(command.data.name, command);
-    } else {
-      console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
-    }
-  }
+	const commandsPath = path.join(foldersPath, folder);
+	const commandFiles = fs.readdirSync(commandsPath).filter((file) => file.endsWith(".js"));
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+		if ("data" in command && "execute" in command) {
+			client.commands.set(command.data.name, command);
+		} else {
+			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+		}
+	}
 }
 
 const eventsPath = path.join(__dirname, "src/events");
 const eventFiles = fs.readdirSync(eventsPath).filter((file) => file.endsWith(".js"));
 
 for (const file of eventFiles) {
-  const filePath = path.join(eventsPath, file);
-  const event = require(filePath);
-  if (event.once) {
-    client.once(event.name, (...args) => event.execute(...args));
-  } else {
-    client.on(event.name, (...args) => event.execute(...args));
-  }
+	const filePath = path.join(eventsPath, file);
+	const event = require(filePath);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
 }
 
 client.once(Events.ClientReady, (readyClient) => {
-  RedditPost.sync();
-  console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+	RedditPost.sync();
+	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
 });
 
 client.login(process.env.TOKEN);
 
 // 0 14 * * * is 7am MT/2pm UTC. Server is in UTC.
 cron.schedule("0 14 * * *", async () => {
-  await createGameThread(client);
+	await createGameThread(client);
 });
 
 const redditFeed = new RedditFeed();
 // * * * * * is every minute
 cron.schedule("* * * * *", () => {
-  redditFeed.getNewPosts(client);
+	redditFeed.getNewPosts(client);
 });
 
 client.on(Events.MessageCreate, async (message) => {
-  if (message.author.bot) {
-    return;
-  }
+	if (message.author.bot) {
+		return;
+	}
 
-  if (message.content.toLowerCase() === "where thread") {
-    message.reply(sarcasmify("Where thread"));
+	if (message.content.toLowerCase() === "where thread") {
+		message.reply(sarcasmify("Where thread"));
 
-    // only create GDT if it's at least 7am on game day
-    if (new Date().getUTCHours() < 14) {
-      message.reply("https://i.imgur.com/rY09A7Z.jpeg")  // todo: store locally and embed as image
-    } else {
-      await createGameThread(client);
-    }
-  }
+		// only create GDT if it's at least 7am on game day
+		if (new Date().getUTCHours() < 14) {
+			message.reply("https://i.imgur.com/rY09A7Z.jpeg"); // todo: store locally and embed as image
+		} else {
+			await createGameThread(client);
+		}
+	}
 
-  if (message.content.toLowerCase() === "first") {
-    if (message.channel.isThread() && message.channel.parent === process.env.THREADS_CHANNEL) {
-      message.reply(sarcasmify("first"));
-    }
-  }
+	if (message.content.toLowerCase() === "first") {
+		if (message.channel.isThread() && message.channel.parent === process.env.THREADS_CHANNEL) {
+			message.reply(sarcasmify("first"));
+		}
+	}
 
-  console.log(message.content);
+	console.log(message.content);
 });
